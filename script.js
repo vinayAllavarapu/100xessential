c40ee1d6ab291f3d106159d9faeae82718010562
Vinay <Vinayallavarapu0@gmail.com>
2023-08-30 20:04:56 +0530

    added 3 files for foundations

---
+++ script.js
@@ -0,0 +1,34 @@
+function sayHello(name){
+    console.log("hello "+ name +"!");
+}
+sayHello("vinay");
+sayHello("reshma");
+sayHello("sridher");
+
+function math(a,b){
+    let sum = a+b;
+    let sub = a-b;
+    let mul = a*b;
+    let div = a/b;
+
+    console.log(sum);
+    console.log(sub);
+    console.log(mul);
+    console.log(div);
+}
+
+math(5,3);
+math(22,7);
+
+const year = 2001;
+
+let food = ["biryani", "paneer", "panipuri"];
+
+let books = {
+    name : "Can't hurt me",
+    author: "David goggins",
+    Published : "2015"
+};
+
+
+
